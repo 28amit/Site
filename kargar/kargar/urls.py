21116d1a95e10de18e214b6ccde6a90627a26c76
28amit <amit28amical@gmail.com>
2021-03-24 10:08:05 +0530

    Update urls.py

--- kargar/kargar/urls.py
+++ kargar/kargar/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(('womenlab.urls','womenlab'),namespace='womenlab')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
